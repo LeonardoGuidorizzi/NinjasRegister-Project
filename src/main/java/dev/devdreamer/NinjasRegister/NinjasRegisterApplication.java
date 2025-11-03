5ec94935d57dc4e1d58ddfb4b6784759d2620711
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-11-03 14:33:02 -0300

    first commit

---
+++ src/main/java/dev/devdreamer/NinjasRegister/NinjasRegisterApplication.java
@@ -0,0 +1,13 @@
+package dev.devdreamer.NinjasRegister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinjasRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinjasRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/devdreamer/NinjasRegister/NinjasRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.devdreamer.NinjasRegister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinjasRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
