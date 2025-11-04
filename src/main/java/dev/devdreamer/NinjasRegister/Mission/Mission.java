7361bc308074a900aff55ee7c8f9a77fc0de751a
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-11-04 18:53:06 -0300

    feat: place lombok in the class to abstract

--- src/main/java/dev/devdreamer/NinjasRegister/Mission/Mission.java
+++ src/main/java/dev/devdreamer/NinjasRegister/Mission/Mission.java
@@ -2,9 +2,16 @@ package dev.devdreamer.NinjasRegister.Mission;
 
 import dev.devdreamer.NinjasRegister.Ninja.Ninja;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 @Table(name = "tb_mission")
 public class Mission {
@@ -18,37 +25,4 @@ public class Mission {
     //uma missão pode ter varios ninjas(uma missão vai ter varios ninjas)
     @OneToMany(mappedBy = "mission")
     private List<Ninja> ninjas;
-
-    public Mission() {
-    }
-
-    public Mission(Long id, String name, String level) {
-        this.id = id;
-        this.name = name;
-        this.level = level;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRank() {
-        return level;
-    }
-
-    public void setRank(String level) {
-        this.level = level;
-    }
 }
--- src/main/java/dev/devdreamer/NinjasRegister/Ninja/Ninja.java
+++ src/main/java/dev/devdreamer/NinjasRegister/Ninja/Ninja.java
@@ -2,9 +2,14 @@ package dev.devdreamer.NinjasRegister.Ninja;
 
 import dev.devdreamer.NinjasRegister.Mission.Mission;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 @Entity
 @Table(name = "tb_ninja")
 public class Ninja {
@@ -19,37 +24,4 @@ public class Ninja {
     @JoinColumn(name = "mission_id")
     private Mission mission;
 
-    public Ninja() {
-    }
-
-    public Ninja(String nome, String email, int age) {
-        this.nome = nome;
-        this.email = email;
-        this.age = age;
-    }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
