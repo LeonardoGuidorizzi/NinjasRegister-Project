56f5abfda2105e8bf54e64dadf8bdf21c6157f95
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-11-04 18:57:41 -0300

    fix: place the email as unique

--- src/main/java/dev/devdreamer/NinjasRegister/Ninja/Ninja.java
+++ src/main/java/dev/devdreamer/NinjasRegister/Ninja/Ninja.java
@@ -17,6 +17,7 @@ public class Ninja {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @Column(unique = true)
     private String email;
     private int age;
     //Um ninja pode ter apenas uma missão(muitos ninjas vai ter uma missão)
