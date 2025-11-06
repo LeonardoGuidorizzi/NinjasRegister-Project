27d4655a55a90406d591ee4c1e3ff8d3ff1aa723
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-11-06 12:56:23 -0300

    feat: add mock of routes to know the methods that i need to do after

--- src/main/java/dev/devdreamer/NinjasRegister/Ninja/NinjaController.java
+++ src/main/java/dev/devdreamer/NinjasRegister/Ninja/NinjaController.java
@@ -1,14 +1,32 @@
 package dev.devdreamer.NinjasRegister.Ninja;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //eu mapeio pra esse arquivo para ser um controller
 @RequestMapping // dizendo que esse arquivo é o mapa de todas as rotas
 public class NinjaController {
+
+
+    @PostMapping()
+    public String create(){
+        return "createNinja";
+    }
+
     @GetMapping("/helloWorld") //rota
     public String helloWord(){
         return "Hello World";
+    }
+
+    @PutMapping
+    public String update(){
+        return "updateNinja";
+    }
+    @DeleteMapping
+    public String deleteById() {
+        return "deleteNinja";
+    }
+    @GetMapping()
+    public String allById(){
+        return  "allById";
     }
 }
