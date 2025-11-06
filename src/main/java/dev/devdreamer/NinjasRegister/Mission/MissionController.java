cede9843b15e64f85db88876f6762d8ddb7ae397
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-11-06 17:52:56 -0300

    fix: change the ninja's and the mission's route

--- src/main/java/dev/devdreamer/NinjasRegister/Mission/MissionController.java
+++ src/main/java/dev/devdreamer/NinjasRegister/Mission/MissionController.java
@@ -1,4 +1,27 @@
 package dev.devdreamer.NinjasRegister.Mission;
 
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("api/mission")
 public class MissionController {
+
+    @PostMapping()
+    public String create(){
+        return "createNinja";
+    }
+
+    @GetMapping() //rota
+    public String getAll(){
+        return "All missions";
+    }
+
+    @PutMapping
+    public String update(){
+        return "updateNinja";
+    }
+    @DeleteMapping
+    public String deleteById() {
+        return "deleteNinja";
+    }
+
 }
--- src/main/java/dev/devdreamer/NinjasRegister/Ninja/NinjaController.java
+++ src/main/java/dev/devdreamer/NinjasRegister/Ninja/NinjaController.java
@@ -3,7 +3,7 @@ package dev.devdreamer.NinjasRegister.Ninja;
 import org.springframework.web.bind.annotation.*;
 
 @RestController //eu mapeio pra esse arquivo para ser um controller
-@RequestMapping // dizendo que esse arquivo é o mapa de todas as rotas
+@RequestMapping("api/ninja") // dizendo que esse arquivo é o mapa de todas as rotas
 public class NinjaController {
 
 
