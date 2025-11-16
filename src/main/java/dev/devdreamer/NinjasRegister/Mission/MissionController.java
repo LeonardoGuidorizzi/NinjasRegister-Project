8d5c365b9f02e52bea6012107ef6afda7fcda940
LeonardoGuidorizzi <leonardo.tesoledy@gmail.com>
2025-11-16 15:24:17 -0300

    doc: add swagger in my application for documentation of my mission's and ninja's routes

--- src/main/java/dev/devdreamer/NinjasRegister/Mission/MissionController.java
+++ src/main/java/dev/devdreamer/NinjasRegister/Mission/MissionController.java
@@ -2,6 +2,7 @@ package dev.devdreamer.NinjasRegister.Mission;
 
 
 import dev.devdreamer.NinjasRegister.Mission.dto.MissionDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,22 +19,25 @@ public class MissionController {
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Cria um nova missão", description = "Essa rota cria um nova missão")
     public MissionDTO create(@RequestBody MissionDTO missionDTO){
         return missionService.create(missionDTO);
     }
 
     @GetMapping() //rota
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Lista todas as missões", description = "Essa rota lista todas as missões do banco")
     public List<MissionDTO> getAll(){
         return missionService.getAll();
     }
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Atualiza uma missão existente", description = "Essa rota atualiza a missão por inteiro")
     public MissionDTO update( @PathVariable Long id, @RequestBody MissionDTO missionDTO){
         return missionService.update(missionDTO, id);
     }
-
+    @Operation(summary = "Atualiza uma missão existente", description = "Essa rota atuliza campos especificos de uma missão")
     @PatchMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public MissionDTO partialUpdate(@PathVariable Long id, @RequestBody MissionDTO missionDTO){
@@ -41,6 +45,7 @@ public class MissionController {
     }
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Operation(summary = "Deleta a missão por id", description = "Essa rota deleta uma missão")
     public void deleteById(@PathVariable Long id ) {
         missionService.deleteById(id);
     }
